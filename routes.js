ea71da02325ff3d2212d99b1e4a4c5ae435f3891
purkerss <purkerss@gmail.com>
2018-07-21 01:39:29 +0000

    Insert SQL cleared, must parse req.body and assign to variables to complete assignment

--- routes.js
+++ routes.js
@@ -1,7 +1,6 @@
 // routes.js
 
-module.exports = function(app, db) {
-
+module.exports 	= function(app, db) {
 const sqlite3 	= require('sqlite3').verbose();
 
 //Create DB Object
@@ -19,27 +18,28 @@ let blogdb = new sqlite3.Database('./blog.db', sqlite3.OPEN_READWRITE, (err) => 
 })
         app.get('/posts', function (req, res) {
 
-let posts = 'SELECT post_id ID, title Title, body Body FROM posts ORDER BY ID';
-
-blogdb.get(posts, (err, row) => {
-	if (err) {
-                return console.error(err.message);
+		let posts = 'SELECT post_id ID, title Title, body Body FROM posts ORDER BY ID';
+		blogdb.get(posts, (err, row) => {
+			if (err) {
+                		return console.error(err.message);
  }
-        // return row
-        // ? console.log(row.ID, row.Title, row.Body)
-        // : console.log('No Blog posts found.')
-	res.send(row); 
+        			// return row
+        			// ? console.log(row.ID, row.Title, row.Body)
+        			// : console.log('No Blog posts found.')
+				res.send(row); 
 });
 
 })
         app.post('/post', function (req, res) {
 
-                blogdb.run('INSERT INTO posts(title,body) VALUES(req.title, req.body)', [title, body], function(err) {
+                blogdb.run("INSERT INTO posts(title, body) VALUES('Testing Title', 'Testing Body')", function(err) {
 			if (err) {
-      				return console.log(err.message);
-    }});
+      				return console.log(err.message)
+				res.send(err.message);
+    }
+				res.send('Blog post received.')
+});
 
                 console.log(req.body)
-                res.send('Blog post received.')
 });
 };
